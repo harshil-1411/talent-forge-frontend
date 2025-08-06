b808bebb94f371216234dec16bf9688b1617ac12
harshil-1411 <164999918+harshil-1411@users.noreply.github.com>
2025-08-07 00:24:15 +0530

    final changes

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -5,23 +5,6 @@ const newRequest = axios.create({
   withCredentials: true,
 });
 
-// Add an interceptor to include the token in requests
-// newRequest.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("accessToken");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });om "axios";
 
-
-// Add an interceptor to include the token in requests
-// newRequest.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("accessToken");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
 
 export default newRequest;
